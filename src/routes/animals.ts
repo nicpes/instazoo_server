b632812e08472337db592ad43a865adb8716f69e
Davide <davidericca109@gmail.com>
2022-07-28 18:03:03 +0200

    routes POST PATCH DELETE

---
+++ src/routes/animals.ts
@@ -0,0 +1,104 @@
+import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const router: Router = Router();
+
+//#region GET ANMALS
+
+// GET ALL ANIMALS
+router.get("/all", async (req, res, next) => {
+  try {
+    const animals = await prisma.animals.findMany();
+
+    res.json(animals);
+  } catch (error) {
+    res.status(400).json(error);
+    return next(error);
+  }
+});
+
+// GET ANIMALS BY ID
+router.get("/:id(\\d+)", async (req, res, next) => {
+  try {
+    const animalId = Number(req.params.id);
+
+    const animal = await prisma.animals.findUnique({ where: { id: animalId } });
+
+    if (!animal) {
+      res.status(404);
+      return next("cannot get this animal");
+    }
+
+    res.json(animal);
+  } catch (error) {
+    res.status(400).json(error);
+    return next(error);
+  }
+});
+
+//#endregion
+
+//#region POST ANIMALS
+
+// POST ANIMALS BY ID
+router.post("/", async (req, res, next) => {
+  try {
+    const animalData = req.body;
+    const animal = await prisma.animals.create({
+      data: animalData,
+    });
+
+    res.status(201).json(`Correctly added animal ID: ${animal}`);
+  } catch (error) {
+    res.status(400).json(error);
+    return next(error);
+  }
+});
+
+//#endregion
+
+//#region PATCH ANIMALS
+
+// PATCH ANIMALS BY ID
+router.patch("/:id(\\d+)", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const animal = await prisma.animals.update({
+      where: { id: Number(id) },
+      data: req.body,
+    });
+
+    res.status(201).json(`Correctly updated Animal ID: ${animal.id}`);
+  } catch (error) {
+    res.status(400).json(error);
+    return next(error);
+  }
+});
+
+//#endregion
+
+//#region DELETE ANIMALS
+
+// DELETE ANIMALS BY ID
+router.delete("/:id(\\d)", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const animal = await prisma.animals.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    res.status(200).json(`Correctly deleted animal ID: ${animal.id}`);
+  } catch (error) {
+    res.status(400).json(error);
+    return next(error);
+  }
+});
+
+//#endregion
+
+router.patch("/");
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,15 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { PrismaClient } from "@prisma/client";
+import animals from "./routes/animals";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
-const prisma = new PrismaClient();
 
-app.get("/animals", async (request, response) => {
-  const animals = await prisma.animals.findMany();
-
-  response.json(animals);
-});
-
-app.get("/animals/:id(\\d+)", async (request, response, next) => {
-  const animalId = Number(request.params.id);
-  const animal = await prisma.animals.findUnique({
-    where: { id: animalId },
-  });
-
-  if (!animal) {
-    response.status(404);
-    return next("cannot get this animal");
-  }
-
-  response.json(animal);
-});
-
-app.post("/animals", async (request, response) => {
-  const animalData = request.body;
-  const animal = await prisma.animals.create({
-    data: animalData,
-  });
-
-  response.status(201).json(animal);
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/animals", animals);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
