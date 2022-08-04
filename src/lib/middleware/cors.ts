c6ec25fa366c280478f527ae03782c675f6580bc
Davide <davidericca109@gmail.com>
2022-08-04 11:13:27 +0200

    fix cors

--- src/lib/middleware/cors.ts
+++ src/lib/middleware/cors.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 export const initCorsMiddleware = () => {
   const corsOptions = {
     origin: "http://localhost:3000",
+    credentials: true,
   };
 
   return cors(corsOptions);
--- src/lib/middleware/passport.ts
+++ src/lib/middleware/passport.ts
@@ -9,12 +9,12 @@ const githubStrategy = new passportGithub2.Strategy(
     clientSecret: config.GITHUB_CLIENT_SECRET,
     callbackURL: config.GITHUB_CALLBACK_URL,
   },
-  (
+  function (
     accessToken: string,
     refreshToken: string,
     profile: { [key: string]: string },
     done: (error: null, user: Express.User) => void
-  ) => {
+  ) {
     const user: Express.User = {
       username: profile.username,
     };
